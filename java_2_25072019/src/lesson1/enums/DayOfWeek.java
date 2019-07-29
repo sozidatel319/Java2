fbb0ff19600a236cbcfd333ab5d878b22c8f9e80
sozidatel319 <43828987+sozidatel319@users.noreply.github.com>
2019-07-29 14:48:18 +0300

    Переделал задание с enum!

--- java_2_25072019/src/lesson1/enums/DayOfWeek.java
+++ java_2_25072019/src/lesson1/enums/DayOfWeek.java
@@ -2,22 +2,16 @@ package lesson1.enums;
 
 public enum DayOfWeek {
 
-    MONDAY(8,1), TUESDAY(8,2), WEDNESDAY(8,3),
-    THURSDAY(8,4), FRIDAY(8,5), SATURDAY(0,6), SUNDAY(0,7);
+    MONDAY(8), TUESDAY(8), WEDNESDAY(8),
+    THURSDAY(8), FRIDAY(8), SATURDAY(0), SUNDAY(0);
 
     private int workhours;
-    private int numberOfDay;
 
-    DayOfWeek(int workhours, int numberOfDay) {
+    DayOfWeek(int workhours) {
         if (workhours >= 0) this.workhours = workhours;
-        if (numberOfDay > 0 && numberOfDay < 8) this.numberOfDay = numberOfDay;
     }
 
     public int getWorkhours() {
         return workhours;
-    }
-
-    public int getNumberOfDay() {
-        return numberOfDay;
     }
 }
--- java_2_25072019/src/lesson1/enums/DayofWeekMain.java
+++ java_2_25072019/src/lesson1/enums/DayofWeekMain.java
@@ -3,13 +3,13 @@ package lesson1.enums;
 public class DayofWeekMain {
 
     public static void main(String[] args) {
-        getWorkingHours(DayOfWeek.FRIDAY);
+        getWorkingHours(DayOfWeek.SUNDAY);
 
     }
 
     public static void getWorkingHours(DayOfWeek dayOfWeek) {
         int hourstoRest = 0;
-        int day = dayOfWeek.getNumberOfDay();
+        int day = dayOfWeek.ordinal() + 1;
 
         if (day < 6) {
             while (day < 6) {
