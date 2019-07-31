5c429c775cf01937573c47eeeba416437679ceeb
sozidatel319 <43828987+sozidatel319@users.noreply.github.com>
2019-07-31 09:06:28 +0300

    Внёс корректировку в выдачу ошибкой MyArraySize сообщения.

--- Lesson2/src/com/company/Main.java
+++ Lesson2/src/com/company/Main.java
@@ -7,14 +7,12 @@ public class Main {
         String[][] arr = {{"5", "2", "3", "4"},
                 {"1", "2", "4", "5"},
                 {"1", "2", "4", "4"},
-                {"1", "2", "3", "2"}};
+                {"1", "2", "3", "4"}};
         try {
             method(arr);
         } catch (MyArraySizeException a) {
-            System.out.println("Поймал MyArraySize");
             a.printStackTrace();
         } catch (MyArrayDataExeption e) {
-            System.out.println("Поймал MyArrayDataExeption");
             e.printStackTrace();
         }
     }
@@ -22,7 +20,7 @@ public class Main {
     public static void method(String[][] arr) throws MyArrayDataExeption, MyArraySizeException {
 
         int sum = 0;
-        int count = 0;
+        int count;
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
@@ -34,7 +32,7 @@ public class Main {
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i].length != 4 || arr.length != 4) throw new MyArraySizeException();
+                if (arr[i].length != 4 || arr.length != 4) throw new MyArraySizeException("Поймал MyArraySize");
                 try {
                     count = Integer.parseInt(arr[i][j]);
                 } catch (NumberFormatException e) {
--- Lesson2/src/com/company/MyArraySizeException.java
+++ Lesson2/src/com/company/MyArraySizeException.java
@@ -4,4 +4,8 @@ import java.io.FileNotFoundException;
 
 public class MyArraySizeException extends RuntimeException {
 
+    public MyArraySizeException(String s){
+        super(s);
+    }
+
 }
