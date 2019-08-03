d475f3e77179baadd30b76bba611de3fa7ea6bab
sozidatel319 <43828987+sozidatel319@users.noreply.github.com>
2019-08-03 13:57:07 +0300

    Решил задачу №1

--- Lesson3/src/com/company/Main.java
+++ Lesson3/src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String [] words = new String[]{"привет","ежик","привет","лес","пресс","кекс",
+        "река","мел","соль","ртуть","ежик","сель","ель","мышь","лес","пресс","кекс",
+        "роль","тополь","собака"};
+
+       HashMap<String,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < words.length; i++) {
+
+            String s = words[i];
+            Integer curentNum = map.get(s);
+            if (curentNum == null){
+                curentNum = 1;
+            }else {
+                ++curentNum;
+            }
+            map.put(s,curentNum);
+
+        }
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            if (entry.getValue() == 1)System.out.println(entry.getKey());
+        }
     }
 }
