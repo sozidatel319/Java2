42a8a382f52171c4a1ab0b56ff6e72a8b254bff6
sozidatel319 <43828987+sozidatel319@users.noreply.github.com>
2019-08-11 21:55:00 +0300

    изменил формулу во втором методе

--- Lesson5/src/com/company/Main.java
+++ Lesson5/src/com/company/Main.java
@@ -26,7 +26,7 @@ public class Main {
 
     }
 
-    public static void method2() {
+    public synchronized static void method2() {
 
         final int size = 10000000;
         final int h = size / 2;
@@ -47,27 +47,27 @@ public class Main {
 
         Thread thread1 = new Thread(() -> {
             for (int i = 0; i < arr1.length; i++) {
-                arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 10) * Math.cos(0.2f + i/ 10) * Math.cos(0.4f + i / 10));
             }
-            System.arraycopy(arr1,0,arr,0,h);
+            System.arraycopy(arr1, 0, arr, 0, h);
         });
 
         Thread thread2 = new Thread(() -> {
             for (int i = 0; i < arr2.length; i++) {
-                arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 10) * Math.cos(0.2f + i / 10) * Math.cos(0.4f + i / 10));
             }
-            System.arraycopy(arr2,0,arr,h,h);
+            System.arraycopy(arr2, 0, arr, h, h);
         });
 
         thread1.start();
         thread2.start();
+
         try {
             thread1.join();
             thread2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
 
         System.out.println(System.currentTimeMillis() - a);
     }
